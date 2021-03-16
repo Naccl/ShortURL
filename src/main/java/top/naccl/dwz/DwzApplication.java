5b47c1884fbb10ad241aad116f3c02ce4b7e47f4
Naccl <i@naccl.top>
2021-03-16 17:13:25 +0800

    :tada: Initial commit.

---
+++ src/main/java/top/naccl/dwz/DwzApplication.java
@@ -0,0 +1,13 @@
+package top.naccl.dwz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/naccl/dwz/DwzApplicationTests.java
@@ -0,0 +1,13 @@
+package top.naccl.dwz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
